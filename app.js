fb42da0fe19255279daaaf2d1f9a8b44a97743f1
Anisa <anisahamohamed@gmail.com>
2020-04-25 17:49:39 -0400

    server and database connection

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
 const path = require("path");
+//DATABASE
+const db = require('./config/database')
+//TEST DATABASE
+db.authenticate()
+.then(()=>console.log('Database Connected ....'))
+.catch(err => console.log('Error' + err))
 
 const app = express();
 
-const PORT = proces.env.PORT || 5000
+app.get('/',(req,res) => res.send('INDEX'));
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log("server started listenning on port {PORT}"));
+app.listen(PORT, console.log(`server started listening on port ${PORT}`));
